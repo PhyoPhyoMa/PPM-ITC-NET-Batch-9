a27ed7a3329ef2e72b2399008a7efa645d74f1b4
unknown <phyophyoma18@gmail.com>
2022-02-07 22:41:15 +0630

    Initial Program By PPM

---
+++ Day2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main(string[] args)       
+        {
+            //Decision Making
+            //string input;
+            //Console.Write("Enter a number : ");
+            //input = Console.ReadLine();
+            //int num;
+            //if(!int.TryParse(input , out num))
+            //{
+            //    Console.WriteLine("Please Enter a number : ");
+            //}
+            //else
+            //{
+            //    int l = Convert.ToInt32(input);
+            //    if (l % 2 == 0)
+            //    {
+            //        Console.WriteLine("This number is Even!");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("This number is Odd!");
+            //    }
+            //}
+            Console.Write("Enter a start number : ");
+            int s = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("Enter a end number : ");
+            int e = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            for(int i = s; i <= e;i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine("Sum : " + sum);
+
+        }
+    }
+}
---
+++ ITC_B9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Apple;
+
+namespace ITC_B9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to ITC");
+            Console.WriteLine("Welcome to ITC");
+            Console.WriteLine("Welcome to ITC");
+
+            Console.Write("Hello World");
+            Console.Write("Hello World");
+            Console.Write("Hello World");
+        }
+    }
+}
