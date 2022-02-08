c1af67a07dda06e4f0b7912574350aa7f7d5236c
unknown <phyophyoma18@gmail.com>
2022-02-08 14:46:09 +0630

    Commit By PPM

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -10,6 +10,12 @@ namespace Day4
     {
         static void Main(string[] args)
         {
+            //Escape Character
+            string directory = @"C:\Windows\Users\newFolder";
+            //String Concatenate
+            Console.WriteLine("Link : " + directory);
+            Console.WriteLine("Link : {0}", directory);
+            Console.WriteLine($"Link : {directory}");
         }
     }
 }
